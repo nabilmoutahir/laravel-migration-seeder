89ca01ae9cc45337f57aa0958e840b4eab92003b
nabilmoutahir <nabil.moutahir97@gmail.com>
2024-03-26 04:39:06 +0100

    add import controller

--- routes/web.php
+++ routes/web.php
@@ -1,6 +1,8 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
+// IMPORT CONTROLLER
+use App\Http\Controllers\Guest\PageController;
 
 /*
 |--------------------------------------------------------------------------
@@ -13,6 +15,4 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::get('/', function () {
-  return view('home');
-})->name('home');
+Route::get('/', [PageController::class,'index'])->name('home');
