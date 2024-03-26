6f69b5e8fe0f8bd051a3b6046d39ea6470053cd9
nabilmoutahir <nabil.moutahir97@gmail.com>
2024-03-26 04:54:02 +0100

    add layout and display database elements

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -3,9 +3,36 @@
 @section('title', 'Pagina iniziale')
 
 @section('main-content')
-  <section>
-    <div class="container py-4">
-      <h1>Laravel with Bootstrap + Vite</h1>
+  <div class="row">
+    
+    {{-- FOR EACH --}}
+    @foreach ($trains as $train)
+        
+    <div class="col-4">
+      <div class=" card">
+        <div class="card-body">
+          <div>
+            <span>Numero Treno:</span>
+            <span>{{$train->id}}</span>
+          </div>
+          <div>
+            <span>Compagnia:</span>
+            <span>{{$train->company}}</span>
+          </div>
+          <div>
+            <span>Partenza:</span>
+            <span>{{$train->departure_station}}</span>
+          </div>
+          <div>
+            <span>Arrivo:</span>
+            <span>{{$train->arrival_station}}</span>
+          </div>
+        </div>
+      </div>
     </div>
-  </section>
+
+    @endforeach
+
+
+  </div>
 @endsection
