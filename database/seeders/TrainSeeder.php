b69109aeeafe5ce182776c3b17c738c331afefdb
nabilmoutahir <nabil.moutahir97@gmail.com>
2024-03-26 20:21:55 +0100

    add train seeder and import faker

---
+++ database/seeders/TrainSeeder.php
@@ -0,0 +1,22 @@
+<?php
+
+namespace Database\Seeders;
+
+use Illuminate\Database\Console\Seeds\WithoutModelEvents;
+use Illuminate\Database\Seeder;
+
+// IMPORT FAKER
+use Faker\Generator as Faker;
+
+class TrainSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     *
+     * @return void
+     */
+    public function run(Faker $faker)
+    {
+        
+    }
+}
