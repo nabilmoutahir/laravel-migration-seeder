20801c7120ce5c5e18475b387b9076051d8a95ad
nabilmoutahir <nabil.moutahir97@gmail.com>
2024-03-26 21:23:07 +0100

    add and fix layout

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -3,36 +3,36 @@
 @section('title', 'Pagina iniziale')
 
 @section('main-content')
-  <div class="row">
-    
+
+<div class="container">
+  <table class="table">
+    <thead>
+      <tr>
+        <th scope="col">ID</th>
+        <th scope="col">Company</th>
+        <th scope="col">Departure Station</th>
+        <th scope="col">Arrival Station</th>
+        <th scope="col">Departure Time</th>
+        <th scope="col">Arrival Time</th>
+        <th scope="col">Train Code</th>
+      </tr>
+    </thead>
+
     {{-- FOR EACH --}}
     @foreach ($trains as $train)
-        
-    <div class="col-4">
-      <div class=" card">
-        <div class="card-body">
-          <div>
-            <span>Numero Treno:</span>
-            <span>{{$train->id}}</span>
-          </div>
-          <div>
-            <span>Compagnia:</span>
-            <span>{{$train->company}}</span>
-          </div>
-          <div>
-            <span>Partenza:</span>
-            <span>{{$train->departure_station}}</span>
-          </div>
-          <div>
-            <span>Arrivo:</span>
-            <span>{{$train->arrival_station}}</span>
-          </div>
-        </div>
-      </div>
-    </div>
-
+      <tbody>
+        <tr>
+          <th>{{$train->id}}</th>
+          <td>{{$train->company}}</td>
+          <td>{{$train->departure_station}}</td>
+          <td>{{$train->arrival_station}}</td>
+          <td>{{$train->departure_time}}</td>
+          <td>{{$train->arrival_time}}</td>
+          <td>{{$train->train_code}}</td>
+        </tr>
+      </tbody>
     @endforeach
-
-
-  </div>
+  </table>
+</div>
+  
 @endsection
--- resources/views/partials/header.blade.php
+++ resources/views/partials/header.blade.php
@@ -1,6 +1,7 @@
 <header>
   <nav class="navbar navbar-expand-lg bg-body-tertiary">
     <div class="container">
+      <h1 class="pe-5">TRAINS</h1>
       <div class="collapse navbar-collapse" id="navbarSupportedContent">
         <ul class="navbar-nav me-auto mb-2 mb-lg-0">
           <li class="nav-item">
